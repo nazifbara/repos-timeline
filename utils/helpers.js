7daf161ad6cf8090bf87e95be6df05b70e6e273a
nazifbara <nazifbarassounon@gmail.com>
2022-02-06 10:19:05 +0100

    zero pad the dates

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,9 @@
 export function formatDate(datetime) {
   const date = new Date(datetime)
-  const [year, month, day] = [date.getFullYear(), date.getMonth() + 1, date.getDate()]
+  const [year, month, day] = [
+    date.getFullYear(),
+    (date.getMonth() + 1).toString().padStart(2, 0),
+    date.getDate().toString().padStart(2, 0),
+  ]
   return `${year}-${month}-${day}`
 }
