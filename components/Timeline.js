62a3c46ade02d25b2ed69958063dce85344cd5b8
nazifbara <nazifbarassounon@gmail.com>
2022-02-23 19:28:03 +0100

    decrease archived repos opacity - fixes #17

--- components/Timeline.js
+++ components/Timeline.js
@@ -7,7 +7,7 @@ export default function Timeline({ repositories, username }) {
   return (
     <div className={styles.timeline}>
       {repositories.map((r) => (
-        <article key={r.id} className={styles.timeline__item}>
+        <article key={r.id} className={`${styles.timeline__item} `}>
           <time dateTime={r.createdAt}>{formatDate(r.createdAt)}</time>
           <div className={styles.timeline__separator}>
             <div className={styles.timeline__dot} />
@@ -17,7 +17,7 @@ export default function Timeline({ repositories, username }) {
             href={`https://github.com/${username}/${r.name}`}
             rel="noreferrer"
             target="_blank"
-            className={styles.timeline__content}
+            className={`${styles.timeline__content} ${r.isArchived && 'archived'}`}
           >
             <time dateTime={r.createdAt}>{formatDate(r.createdAt)}</time>
             <div className={`flex ai-c ${styles.timeline__heading}`}>
