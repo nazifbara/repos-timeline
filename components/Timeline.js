46809612fd28b6a762920dbe4db109704c1f0916
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2022-06-01 18:19:02 +0000

    show active time, use momentjs - fixes #19

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,14 +1,14 @@
 import styles from 'styles/Timeline.module.scss'
+import moment from 'moment'
 
 import { StarIcon, ForkIcon } from 'components'
-import { formatDate } from 'utils/helpers'
 
 export default function Timeline({ repositories, username }) {
   return (
     <div className={styles.timeline}>
       {repositories.map((r) => (
         <article key={r.id} className={`${styles.timeline__item} `}>
-          <time dateTime={r.createdAt}>{formatDate(r.createdAt)}</time>
+          <time dateTime={r.createdAt}>{moment(r.createdAt).format('YYYY-MM-DD')}</time>
           <div className={styles.timeline__separator}>
             <div className={styles.timeline__dot} />
             <div className={styles.timeline__connector} />
@@ -19,7 +19,7 @@ export default function Timeline({ repositories, username }) {
             target="_blank"
             className={`${styles.timeline__content} ${r.isArchived && 'archived'}`}
           >
-            <time dateTime={r.createdAt}>{formatDate(r.createdAt)}</time>
+            <time dateTime={r.createdAt}>{moment(r.createdAt).format('YYYY-MM-DD')}</time>
             <div className={`flex ai-c ${styles.timeline__heading}`}>
               {r.isFork && <ForkIcon />}
               <h2>{r.name}</h2>
@@ -27,6 +27,10 @@ export default function Timeline({ repositories, username }) {
                 <StarIcon /> {r.stargazerCount}
               </div>
             </div>
+            <span className={styles.timeline__activeTime}>
+              active time: {moment(r.createdAt).from(r.lastCommitDate, true)} - last commit:{' '}
+              {moment(r.lastCommitDate).fromNow()}
+            </span>
             <p>{r.description}</p>
           </a>
         </article>
