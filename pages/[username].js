057988ee9ff0fb5f8b74e4e2ab187dfaa696a716
nazifbara <nazifbarassounon@gmail.com>
2023-02-01 12:27:25 +0100

    temporarily log public credential

--- pages/[username].js
+++ pages/[username].js
@@ -43,6 +43,8 @@ export default function TimelineView({ user, repositories, error }) {
 }
 
 export async function getServerSideProps({ params: { username } }) {
+  console.log('credential', process.env.GITHUB_TOKEN)
+
   try {
     const { data } = await client.query({
       query: GET_TIMELINE,
